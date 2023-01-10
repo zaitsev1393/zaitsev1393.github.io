7d2197a95400e13a5b2381f642c7d85a4d03e591
zaitsev1393 <zaitsev1393@gmail.com>
2023-01-10 22:14:19 +0200

    Feature - github logo + link added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,16 @@
 import "./navbars.css";
+import githubLogo from "./../assets/img/icons8-github.svg";
 
 const Navbar = ({ name }) => {
+  const onLogoClicked = () => {
+    console.log("clicked");
+    window.open("https://github.com/zaitsev1393");
+  }
   return (
-    <div className = "content"> { name } </div>
+    <div className = "content"> 
+      <div>{ name }</div>
+      <img onClick = { onLogoClicked } className = "logo" src = { githubLogo } alt = "Github" />
+    </div>
   )
 }
 
